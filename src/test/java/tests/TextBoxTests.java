37a0dc3d2bc3bd6137cffa9d93d74808ae03b704
Anastasiia Belevich <anast.belevich@gmail.com>
2026-09-04 23:02:36 +0300

    Закончила писать тест кейс на automation-practice-form

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -2,10 +2,12 @@ package tests;
 
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTests {
@@ -19,6 +21,12 @@ public class TextBoxTests {
         Configuration.timeout = 5000; // default 4000
     }
 
+    @AfterAll
+    static void afterAll() {
+        System.out.println("\n### afterAll()\n");
+    }
+
+
     @Test
     void fillFormTest() {
         open("/automation-practice-form");
@@ -28,7 +36,19 @@ public class TextBoxTests {
         $("[for=gender-radio-2]").click();
         $("#userNumber").setValue("1234567895");
 
-
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__year-select").selectOption("1990");
+        $(".react-datepicker__day--015").click();
+        $("#subjectsInput").setValue("тема");
+        $("#hobbies-checkbox-2").click();
+        $("#hobbies-checkbox-3").click();
+        $("#uploadPicture").uploadFromClasspath("img/cat.jpg");
+        $("#currentAddress").setValue("Адрес");
+        $("#state").click();
+        $(byText("Haryana")).click();
+        $("#city").click();
+        $(byText("Karnal")).click();
 
         $("#submit").click();
 
@@ -36,9 +56,11 @@ public class TextBoxTests {
         $$("tbody tr").findBy(text("Student Email")).shouldHave(text("nasti@mail.ru"));
         $$("tbody tr").findBy(text("Gender")).shouldHave(text("Female"));
         $$("tbody tr").findBy(text("Mobile")).shouldHave(text("1234567895"));
-        //  $("#output #name").shouldHave(text("Alex"));
-        //  $("#output #email").shouldHave(text("alex@egorov.com"));
-        //  $("#output #currentAddress").shouldHave(text("Some street 1"));
-        // $("#output #permanentAddress").shouldHave(text("Another street 1"));
+        $$("tbody tr").findBy(text("Date of Birth")).shouldHave(text("15 April,1990"));
+        $$("tbody tr").findBy(text("Hobbies")).shouldHave(text("Reading, Music"));
+        $$("tbody tr").findBy(text("Picture")).shouldHave(text("cat.jpg"));
+        $$("tbody tr").findBy(text("Address")).shouldHave(text("Адрес"));
+        $$("tbody tr").findBy(text("State and City")).shouldHave(text("Haryana Karnal"));
+
     }
 }
